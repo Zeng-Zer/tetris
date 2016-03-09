534e815022810578e7e303dd30fba2893ce595c3
Plancher <jean.plancher@epitech.eu>
2016-03-09 02:18:01 +0100

    auto

--- screen/aff_screen.c
+++ screen/aff_screen.c
@@ -5,7 +5,7 @@
 ** Login   <planch_j@epitech.net>
 **
 ** Started on  Tue Mar  8 16:08:40 2016 Jean PLANCHER
-** Last update Wed Mar  9 01:24:22 2016 Jean PLANCHER
+** Last update Wed Mar  9 01:48:40 2016 Jean PLANCHER
 */
 
 #include "screen.h"
@@ -20,15 +20,15 @@ static int	get_input(t_setup *setup)
   if (!my_strcmp(touch, setup->quit))
     return (0);
   else if (!my_strcmp(touch, setup->left))
-    printw("left");
+    printw("left : [%s]", setup->left);
   else if (!my_strcmp(touch, setup->right))
-    printw("right");
+    printw("right : [%s]", setup->right);
   else if (!my_strcmp(touch, setup->turn))
-    printw("turn");
+    printw("turn : [%s]", setup->turn);
   else if (!my_strcmp(touch, setup->drop))
-    printw("drop");
+    printw("drop : [%s]", setup->drop);
   else if (!my_strcmp(touch, setup->pause))
-    printw("pause");
+    printw("pause : [%s]", setup->pause);
   return (1);
 }
 
