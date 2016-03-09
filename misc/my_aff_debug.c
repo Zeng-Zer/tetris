5130731b560f921db1f2f0951674d5ed5cd61210
David Zeng <zeng_d@epitech.eu>
2016-03-09 01:42:10 +0100

    Debug

--- misc/my_aff_debug.c
+++ misc/my_aff_debug.c
@@ -5,7 +5,7 @@
 ** Login   <zeng_d@epitech.net>
 **
 ** Started on  Fri Mar  4 18:27:53 2016 David Zeng
-** Last update Wed Mar  9 01:30:10 2016 David Zeng
+** Last update Wed Mar  9 01:41:49 2016 David Zeng
 */
 
 #include "my_fonction.h"
@@ -23,5 +23,4 @@ void		my_aff_debug(t_setup *setup, t_list *tetrimino)
   my_aff_tetrimino(tetrimino);
   my_printf("Press a key to start Tetris\n");
   read(0, NULL, 1);
-  my_init_term(NULL, 1);
 }
